46d714afa4a5482ecf63d9a658c81f3b7e3e74cd
khalid-Nemo <khalid.laksantini@nemo.ma>
2026-01-30 12:23:34 +0100

    handling properly the prompt given to the llm

--- .history/main_20260129182824.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from rag-in-action!")
-from date
-
-if __name__ == "__main__":
-    main()
--- .history/main_20260129182832.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from rag-in-action!")
-from datetime import datetime
-
-if __name__ == "__main__":
-    main()
